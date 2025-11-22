d6ab6fcf56819bdb80fe49376bf28578ea5922d1
Junseo Bae <ferrater1013@gmail.com>
2025-11-22 22:29:51 +0900

    init: 프로젝트 초기 설정

---
+++ build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.spring") version "2.1.10" // = allopen(spring) 설정
+
+	id("org.springframework.boot") version "3.5.7"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("plugin.jpa") version "2.1.10"
+}
+
+group = "com.bebeis"
+version = "0.0.1-SNAPSHOT"
+description = "skill weaving platform"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+
+    maven {
+        name = "embabel-releases"
+        url = uri("https://repo.embabel.com/artifactory/libs-release")
+        mavenContent { releasesOnly() }
+    }
+    maven {
+        name = "embabel-snapshots"
+        url = uri("https://repo.embabel.com/artifactory/libs-snapshot")
+        mavenContent { snapshotsOnly() }
+    }
+}
+
+val embabelAgentVersion = "0.3.0"
+
+dependencies {
+    implementation("com.embabel.agent:embabel-agent-starter:${embabelAgentVersion}")
+    implementation("com.embabel.agent:embabel-agent-starter-openai:${embabelAgentVersion}")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_1)
+	}
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/bebeis/skillweaver/SkillweaverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bebeis.skillweaver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkillweaverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
