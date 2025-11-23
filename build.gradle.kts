c0c74e71071ecaaf7e2b524915d0e2b6721dcae8
Junseo Bae <ferrater1013@gmail.com>
2025-11-23 11:21:11 +0900

    chore: BCrypt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.mindrot:jbcrypt:0.4")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
